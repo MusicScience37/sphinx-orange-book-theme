9c187eb38aa0e5887071ce4519c3d1e87996284a
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2023-12-09 08:14:06 +0000

    Fix error in spell check.

--- docs/start_auto_build.sh
+++ docs/start_auto_build.sh
@@ -7,6 +7,6 @@ cd $(dirname $0)
 ./cpp_example/generate_doxygen_xml.sh
 
 # prevent warning.
-export PYDEVD_DISABLE_FILE_VALIDATION=1
+export PYDEVD_DISABLE_FILE_VALIDATION=1 # cspell:disable-line
 
 sphinx-autobuild source build --port 2765
